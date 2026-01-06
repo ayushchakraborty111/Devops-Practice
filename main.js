9659c693940dba75365a066f99db047722789465
Ayush Chakraborty <ayushchakraborty.cse19@chitkarauniversity.edu.in>
2026-01-06 15:53:00 +0530

    fix: Changed server port to bind to 0.0.0.0 as docker is mapped using this port

--- main.js
+++ main.js
@@ -8,5 +8,6 @@ var server = http.createServer(function (request, response) {
 });
 
 // listen on localhost:8000
-server.listen(8000);
-console.log("Server listening at http://127.0.0.1:8000/");
+server.listen(8000, '0.0.0.0', () => {
+  console.log('Server running on port 8000');
+});
